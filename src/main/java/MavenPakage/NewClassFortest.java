b794c25dd1cbbcf6fc05f4c36998f51c5746251e
jayrajprakash <jayrajprakash@outlook.com>
2024-05-27 15:12:32 -0700

    Second Commit

---
+++ src/main/java/MavenPakage/NewClassFortest.java
@@ -0,0 +1,11 @@
+package MavenPakage;
+
+public class NewClassFortest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcom to Git...!!");
+
+	}
+
+}
